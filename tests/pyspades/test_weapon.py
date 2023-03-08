f9df1d10758661fa4130026bea646ce0fea2a0ce
Yann Savioz <savioz@kth.se>
2023-03-08 15:04:01 +0100

    Corrected with magicMock

--- tests/pyspades/test_weapon.py
+++ tests/pyspades/test_weapon.py
@@ -1,14 +1,15 @@
 from twisted.trial import unittest
 from pyspades import weapon
-from unittest.mock import Mock
+from unittest.mock import MagicMock
 
 class BaseWeaponTest(unittest.TestCase):
     def test_set_shoot(self):
-        wpn = weapon.BaseWeapon.__init__(Mock())
+        wpn = weapon.BaseWeapon.__init__(MagicMock())
 
         shoot_time = wpn.shoot_time
         wpn.set_shoot(True)
-        self.assertNotEqual(wpn.shoot_time, shoot_time)
+        self.assertTrue(wpn.shoot_time != shoot_time)
+
         next_shot = wpn.next_shot
         wpn.set_shoot(False)
-        self.assertNotEqual(wpn.next_shot, next_shot)
+        self.assertTrue(wpn.next_shot != next_shot)
