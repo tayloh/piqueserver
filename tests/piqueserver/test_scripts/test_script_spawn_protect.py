7f1744cfd8c1c451ebdf261e0b9975570b0a5cbd
Yann Savioz <savioz@kth.se>
2023-03-08 16:11:46 +0100

    incorrect tests

---
+++ tests/piqueserver/test_scripts/test_script_spawn_protect.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import MagicMock
+
+import piqueserver.scripts.spawn_protect as spawn_protect
+
+
+import piqueserver.scripts.blockinfo as blockinfo
+
+from piqueserver.player import FeatureConnection
+from piqueserver.server import FeatureProtocol
+
+class BaseSpawnProtectTest(unittest.TestCase):
+
+    def test_spawn_and_hit(self):
+
+        hit_ply = MagicMock()
+        hit_ply.spawn_timestamp = -10
+
+        not_hit_ply = MagicMock()
+        not_hit_ply.spawn_timestamp = 0
+
+        connection = FeatureConnection
+        protocol = FeatureProtocol
+
+
+        spawn_protocol, spawn_connection =  spawn_protect.apply_script(protocol, connection, None)
+
+        spawn_connection.protocol = MagicMock()
+        spawn_connection.on_spawn(spawn_connection, MagicMock())
+
+        self.assertFalse(spawn_connection.on_hit(MagicMock(), 10, not_hit_ply, MagicMock(), MagicMock()))
+        self.assertTrue(spawn_connection.on_hit(MagicMock(), 10, hit_ply, MagicMock(), MagicMock()))
